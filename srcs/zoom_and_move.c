20681e2b454b81804de7fa620776a784b5dc1525
J0NY97 <jony.salmi97@gmail.com>
2021-05-04 16:36:46 +0300

    norminette ready zoom_and_move.c

--- srcs/zoom_and_move.c
+++ srcs/zoom_and_move.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 14:39:16 by nneronin          #+#    #+#             */
-/*   Updated: 2021/05/04 11:46:32 by jsalmi           ###   ########.fr       */
+/*   Updated: 2021/05/04 16:36:26 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 void	zoom_and_move(t_bui_element *elem, t_bui_libui *libui, t_brush *brush)
 {
-	t_xywh new_coord;
+	float		org_w;
+	float		org_h;
+	t_xywh		new_coord;
 	SDL_Surface	*surface;
-	int org_w = elem->position.w;
-	int org_h = elem->position.h;
 
+	org_w = elem->position.h;
+	org_h = elem->position.w;
 	if (mouse_wheel(libui))
 	{
 		new_coord = elem->position;
-		new_coord.h = ((org_w - (libui->mouse_wheel_y * 10)) / ((float)org_w / (float)org_h));
-		new_coord.w = (new_coord.h * ((float)org_w / (float)org_h));
+		new_coord.h = ((org_w - (libui->mouse_wheel_y * 10)) / (org_w / org_h));
+		new_coord.w = (new_coord.h * (org_w / org_h));
 		if (new_coord.h > 10 && new_coord.w > 10)
 		{
 			brush->zoom += libui->mouse_wheel_y * 10;
